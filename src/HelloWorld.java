c1c68d8ee86167f66dfb9a0ee6899e36861d70db
Donelle Harris <donelle.harris@gmail.com>
2020-09-28 17:06:34 -0500

    completed class walkthrough of Intro to Java + Syntax, Types, and Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,36 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, ");
-        System.out.print("World!");
+        System.out.println("World!");
+        int myFavoriteNumber = 11;
+        System.out.println(myFavoriteNumber);
+        String myString = "Today is Monday.";
+        char myChar = 'd';
+        System.out.println(myString);
+        System.out.println(myChar);
+        long myNumber;
+        myNumber = 123L;
+        System.out.println(myNumber);
+        int p = 5;
+        System.out.println(p++);
+        System.out.println(p);
+        int q = 5;
+        System.out.println(++q);
+        System.out.println(q);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+        int n = 4;
+        n += 5;
+System.out.println(n);
+        int x = 3;
+        int y = 4;
+        y *= x;
+System.out.println(y);
+        int b = 10;
+        int c = 2;
+        b /= c;
+        b -= c;
+        System.out.print(b + ", " + c);
     }
 }
