6409e164394f8860fd6f943fc8e40639500f9931
Donelle Harris <donelle.harris@gmail.com>
2020-09-29 14:59:28 -0500

    complete Bonus section of exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 import static java.lang.Integer.parseInt;
 
 public class ConsoleExercises {
@@ -10,35 +9,33 @@ public class ConsoleExercises {
 
         //Scanner class:
         //1.
-        Scanner number = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Please enter a whole number:");
-        int wholeNumber = number.nextInt();
+        int wholeNumber = scan.nextInt();
         System.out.printf("Your number is %s.\n", wholeNumber);
 
         //2.
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter your full name:");
-        String firstName = sc.next();
-        String middleName = sc.next();
-        String lastName = sc.next();
-        System.out.printf("Hello, %s %s %s! \n", firstName, middleName, lastName);
+        System.out.println("Please enter your full name with middle initial:");
+        String firstName = scan.next();
+        String middleInitial = scan.next();
+        String lastName = scan.next();
+        System.out.printf("Hello, %s %s %s! \n", firstName, middleInitial, lastName);
 
         //3.
-        Scanner scanQuote = new Scanner(System.in);
         System.out.println("what is your favorite quote?");
-        String quote = scanQuote.next();
-        System.out.printf("Wow, \"%s\" is a great quote.\n", quote);
-
-        //4.
-        Scanner scanQuote2 = new Scanner(System.in);
-        System.out.println("what is your favorite quote?");
-        String fullQuote = scanQuote2.nextLine();
+        scan.useDelimiter("\n");
+        String fullQuote = scan.next();
+        scan.nextLine();
         System.out.printf("Wow, \"%s\" is a great quote.\n", fullQuote);
 
         //Calculate
-        System.out.println("What is the length and width of the classroom?");
-        String length = sc.next();
-        String width = sc.next();
-        System.out.printf("The area of the classroom is " + Float.parseFloat(length) * Float.parseFloat(width) + ".\n");
+        System.out.println("What is the length, width and height of the classroom?");
+        String length = scan.next();
+        String width = scan.next();
+        String height = scan.next();
+        System.out.printf("The area of the classroom is " + Float.parseFloat(length) * Float.parseFloat(width)
+                + " sf, the perimeter is " + (Float.parseFloat(length) * 2 + Float.parseFloat(width) * 2)
+                + " ft. The volume of the classroom is "
+                + Float.parseFloat(length) * Float.parseFloat(width) * Float.parseFloat(height) + " cf. \n");
     }
 }
