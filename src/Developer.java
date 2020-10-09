a2865177fc5caff4756db1460576f41e126b7feb
Donelle Harris <donelle.harris@gmail.com>
2020-10-09 14:17:50 -0500

    Complete class lecture
    -class example files incomplete & need refactoring
    -delete PolymorphismDemo file

---
+++ src/Developer.java
@@ -0,0 +1,12 @@
+abstract class Developer extends Employee{
+
+    public Developer(String name, String department) {
+        super(name, department);
+    }
+    public Developer(String name) {
+        super(name, "Development");
+    }
+    public String work() {
+        return "writing code";
+    }
+}
--- src/Employee.java
+++ src/Employee.java
@@ -1,27 +1,39 @@
-public class Employee extends Person {
-    public Employee(String employeeName){
-        super(employeeName);
+abstract class Employee {
+    protected String name;
+    protected String department;
+
+    public Employee(String name, String department) {
+        this.name = name;
+        this.department = department;
     }
 
-    private double salary;
-    public void doWork() {
-        System.out.println("Work, work...");
-    }
-    public String work() {
-        return "Filling out TPS reports";
-    }
-    public void sayHello(){
-        System.out.println("How can I help you?");
-    }
-    public static void main(String[] args) {
-        Employee john = new Employee("John");
-        john.sayHello();
-        System.out.println(john.name);
-        Person p = new Person("Joe");
-        Employee e = new Employee("Sally");
+        public String getName() {
+            return name;
+        }
+        public String getDepartment() {
+            return department;
+        }
 
-        p.sayHello();
-        e.sayHello(); // prints "Hello, World!"
-        e.doWork(); // prints "Work, work..."
+        public abstract String work();
     }
-}
+
+
+// From Inheritance & Polymorphism Lesson:
+//    public String work() {
+//        return "Filling out TPS reports";
+//    }
+//    public void sayHello(){
+//        System.out.println("How can I help you?");
+//    }
+//    public static void main(String[] args) {
+//        Employee john = new Employee("John");
+//        john.sayHello();
+//        System.out.println(john.name);
+//        Person p = new Person("Joe");
+//        Employee e = new Employee("Sally");
+//
+//        p.sayHello();
+//        e.sayHello(); // prints "Hello, World!"
+//        e.doWork(); // prints "Work, work..."
+//    }
+//}
